6cbbd87d00a8b8fd753a2a15912501681d6c4273
My project <g2036535@gmail.com>
2026-06-21 22:49:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/note_view.dart';
+
+void main() {
+  runApp( NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const NoteView(
+
+      ),
+    );
+  }
+}
